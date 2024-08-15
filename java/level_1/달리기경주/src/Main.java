20d89761a36a48df774500abc5c29921971729fb
JeongGyuEop <gueeop0219@naver.com>
2024-08-16 00:40:44 +0900

    Programmers java level_1 problem solved

---
+++ java/level_1/달리기경주/src/Main.java
@@ -0,0 +1,38 @@
+package src;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] players =  {"mumu", "soe", "poe", "kai", "mine"};
+        String[] callings = {"kai", "kai", "mine", "mine"};
+        String[] result = solution(players, callings);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    public static String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> playerPositions = new HashMap<>();
+        for(int i = 0; i < players.length; i++) {
+            playerPositions.put(players[i], i);
+        }
+
+        for(String call : callings) {
+            int pos = playerPositions.get(call);
+            if(pos > 0) {
+                String previousPlayer = players[pos - 1];
+                players[pos - 1] = call;
+                players[pos] = previousPlayer;
+
+                // 위치 정보 업데이트
+                playerPositions.put(call, pos - 1);
+                playerPositions.put(previousPlayer, pos);
+
+            }
+        }
+        return players;
+    }
+}
---
+++ javascript/level1/달리기_경주.js
@@ -0,0 +1,12 @@
+const solution = (players, callings) => {
+  for(let i=0; i<callings.length; i++) {
+    if(){}
+  }
+}
+
+
+let players = ["mumu", "soe", "poe", "kai", "mine"];
+let callings = ["kai", "kai", "mine", "mine"];
+
+console.log(solution(players, callings));
+
